ea0df6e7065eb02b46c77bbf155b0f8e70b972d6
ibmpariwesh <pariweshg@gmail.com>
2022-04-19 17:02:29 +0530

    service discovery

--- eureka-service/src/main/java/com/example/demo/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/example/demo/EurekaServiceApplication.java
@@ -2,13 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-@EnableEurekaClient
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 @SpringBootApplication
-public class OrderServiceApplication {
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
